df9f50bfbd066f33dd93792fc188c19cfd98659b
Hideinseek <younglionpics@gmail.com>
2023-01-13 23:32:32 -0500

    fixed upgrader role

--- src/Roles/role.upgrader.js
+++ src/Roles/role.upgrader.js
@@ -1,20 +1,35 @@
-  // the function called run is contained in our roleUpgrader object, and exported to the main module
+ // the function called run is contained in our roleUpgrader object, and exported to the main module
 
-  var roleUpgrader = {
+ var roleUpgrader = {
 
+  
     run: function(creep) {
-    if(creep.store.getFreeCapacity() == 0) {
+        // If creep is upgrading and has no energy it will say harvest and go harvest
+        if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.upgrading = false;
+            creep.say('🔄 harvest');
+	    }
+	    // if creep is not upgrading yet but is full of energy it will say upgrade
+	    if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+	        creep.memory.upgrading = true;
+	        creep.say('⚡ upgrade');
+	    }
+	    
+	    // if creep is upgrading it will go find target
+
+	    if(creep.memory.upgrading) {
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+        }
+        // or else it will find source and switch to harvest
+        else {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
-        else {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
-            }
-        }
-}
+	}
 };
 
 module.exports = roleUpgrader;
